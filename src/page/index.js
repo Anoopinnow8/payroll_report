adf05c5937b2f207b5024d955d89d3afc8acecb0
Anoop <apatel@innow8apps.com>
2024-11-12 12:37:18 +0530

    Update :

--- src/page/index.js
+++ src/page/index.js
@@ -1,18 +1,19 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Papa from "papaparse";
 import Table from "../component/Table";
 import preview from "../assets/image/eye.png";
 import fileimage from "../assets/image/document.png";
-import deleteicon from "../assets/image/delete.png"
+import deleteicon from "../assets/image/delete.png";
 import * as XLSX from "xlsx";
 import { toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css'
+import "react-toastify/dist/ReactToastify.css";
 
 const Main = () => {
   const [uploadFile, setUploadFile] = useState(null);
   const [jsonData, setJsonData] = useState(null);
   const [showUploadFile, setShowUploadFile] = useState(false);
-  const [convertedFileUrl, setConvertedFileUrl] = useState('');
+  const [convertedFileUrl, setConvertedFileUrl] = useState("");
+  const [isConverting, setIsConverting] = useState(false);
   const fileInputRef = useRef(null);
 
   const handleUploadClick = () => {
@@ -21,92 +22,111 @@ const Main = () => {
     }
   };
 
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
 
+    if (file) {
+      if (file.type === "text/csv") {
+        Papa.parse(file, {
+          header: true,
+          complete: (result) => {
+            toast.success("File uploaded successfully");
+            setJsonData(result.data);
+            setUploadFile(file);
+          },
+          error: (error) => {
+            console.error("Error parsing CSV file:", error);
+          }
+        });
+      } else if (
+        file.type ===
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
+        file.name.endsWith(".xlsx")
+      ) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const data = new Uint8Array(e.target.result);
+          const workbook = XLSX.read(data, { type: "array" });
+          const sheetName = workbook.SheetNames[0];
+          const sheet = workbook.Sheets[sheetName];
+          const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
-const handleFileChange = (event) => {
-  const file = event.target.files[0];
-
-  if (file) {
-    if (file.type === "text/csv") {
-      Papa.parse(file, {
-        header: true,
-        complete: (result) => {
-          toast.success('file upload successfully')
-          setJsonData(result.data);
+          setJsonData(jsonData);
           setUploadFile(file);
-         
-        },
-        error: (error) => {
-          console.error("Error parsing CSV file:", error);
-        },
-      });
-    } else if (
-      file.type ===
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
-      file.name.endsWith(".xlsx")
-    ) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const data = new Uint8Array(e.target.result);
-        const workbook = XLSX.read(data, { type: "array" });
-        const sheetName = workbook.SheetNames[0];
-        const sheet = workbook.Sheets[sheetName];
-        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-        
-        setJsonData(jsonData);
-        setUploadFile(file);
-        toast.success('file upload successfully')
-      };
-      reader.onerror = (error) => {
-        console.error("Error reading XLSX file:", error);
-      };
-      reader.readAsArrayBuffer(file);
-    } else {
-      alert("Please upload a CSV or XLSX file.");
+          toast.success("File uploaded successfully");
+        };
+        reader.onerror = (error) => {
+          console.error("Error reading XLSX file:", error);
+        };
+        reader.readAsArrayBuffer(file);
+      } else {
+        alert("Please upload a CSV or XLSX file.");
+      }
     }
-  }
-};
-
+  };
 
   const handleShowPreview = () => {
     setShowUploadFile(true);
   };
+
   const handleRemoveUploaded = () => {
     setUploadFile(null);
     setJsonData(null);
-    setShowUploadFile(false)
+    setShowUploadFile(false);
   };
 
   const handleFileConvert = () => {
     if (!uploadFile) return;
-  
+    setIsConverting(true)
     const url = "https://csv-convertor.onrender.com/convert/";
-  
-   
     const formdata = new FormData();
-   
     formdata.append("input", uploadFile, uploadFile.name);
-  
-    
+
     const requestOptions = {
       method: "POST",
       body: formdata,
       redirect: "follow"
     };
-  
-    console.log(requestOptions,"requestOptions");
-    fetch(url, requestOptions)
-      .then(response => response.text())
-      .then(result => {
-        toast.success('file convert successfully')
-      })
-      .catch(error => {
-        console.log(error,"api error");
-        toast.error('oops!! something went wrong ')
-      });
-  };
-  
 
+    fetch(url, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.file_url) {
+          setConvertedFileUrl(result.file_url);
+          toast.success("File converted successfully");
+        }
+      })
+      .catch((error) => {
+        console.log("API error:", error);
+        toast.error("Oops! Something went wrong.");
+      }
+      )
+      .finally(
+        setIsConverting(false)
+      );
+  };
+
+  useEffect(() => {
+    if (convertedFileUrl) {
+      fetch(convertedFileUrl)
+        .then((response) => response.text())
+        .then((csvData) => {
+          Papa.parse(csvData, {
+            header: true,
+            complete: (result) => {
+              setJsonData(result.data);
+              handleShowPreview();
+            },
+            error: (error) => {
+              console.error("Error parsing converted file:", error);
+            }
+          });
+        })
+        .catch((error) =>
+          console.error("Error fetching converted file:", error)
+        );
+    }
+  }, [convertedFileUrl]);
 
   return (
     <div className="main-container">
@@ -123,40 +143,80 @@ const handleFileChange = (event) => {
             type="file"
             ref={fileInputRef}
             style={{ display: "none" }}
-            accept=".csv"
-            onChange={handleFileChange}
+            accept=".csv, .xlsx"
+            onChange={handleFileUpload}
           />
           {uploadFile && (
             <>
               <div className="upload-data">
-                <span className="file-name"> { <img src={fileimage} alt="fileicon" />}{uploadFile.name}</span>
-                <img
-                  src={preview}
-                  alt="preview"
-                  className="preview"
-                  onClick={handleShowPreview}
-                />
-                 <img
-                  src={deleteicon}
-                  alt="deleteicon"
-                  className="deleteicon"
-                  onClick={handleRemoveUploaded}
-                />
+                <span className="file-name">
+                  {<img src={fileimage} alt="file icon" />}
+                  {uploadFile.name}
+                </span>
+                {!convertedFileUrl && (
+                  <>
+                    <img
+                      src={preview}
+                      alt="preview"
+                      className="preview"
+                      onClick={handleShowPreview}
+                    />
+                    <img
+                      src={deleteicon}
+                      alt="delete icon"
+                      className="deleteicon"
+                      onClick={handleRemoveUploaded}
+                    />
+                  </>
+                )}
               </div>
             </>
           )}
         </div>
-
+        <div className="upload-box">
         <button
           className="button convert"
           disabled={!jsonData}
           onClick={handleFileConvert}
         >
-          Convert
-        </button>
-      </div>
-     {showUploadFile&& <span className="filename">  Preview</span>}
-      {showUploadFile && jsonData && <Table data={jsonData} name={uploadFile.name} />}
+        {isConverting?"Converting...":  "Convert"}
+          </button>
+          {convertedFileUrl && (
+            <>
+              <div className="upload-data">
+                <span className="file-name">
+                  {<img src={fileimage} alt="file icon" />}
+                  {uploadFile.name}
+                </span>
+              
+                  <>
+                    <img
+                      src={preview}
+                      alt="preview"
+                      className="preview"
+                      onClick={handleShowPreview}
+                    />
+                    <img
+                      src={deleteicon}
+                      alt="delete icon"
+                      className="deleteicon"
+                      onClick={handleRemoveUploaded}
+                    />
+                  </>
+               
+              </div>
+            </>
+          )}
+
+        </div>
+        </div>
+
+      {showUploadFile && (
+        <span className="filename">
+          {convertedFileUrl ? "Converted file view" : "Uploaded file view"}
+        </span>
+      )}
+      {showUploadFile && jsonData && <Table data={jsonData} />}
     </div>
   );
 };
