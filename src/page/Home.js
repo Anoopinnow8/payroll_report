e250b2131c1e19e0b12a0313460bc0b102d95e8a
Anoop <apatel@innow8apps.com>
2024-11-29 11:08:59 +0530

    update:

--- src/page/Home.js
+++ src/page/Home.js
@@ -15,6 +15,7 @@ import {
 } from "../api/Function";
 import { convertCsvToJson } from "../utils/ConvertJson";
 import triggerApiRequest from "../api/AutomateApi";
+import { handleLastConvertedTime ,formatDate} from "../utils/Common";
 const Home = () => {
   const navigate = useNavigate();
   const [curTab, setCurTab] = useState("1");
@@ -33,7 +34,6 @@ const Home = () => {
 
   const [isLoading, setisLoading] = useState(false);
   const [isAutomatate, setIsAutomate] = useState(false);
-  const [isAutomatedApiCalled, setIsAutomatedApiCalled] = useState(false);
   const [showCalendar, setshowCalandar] = useState(false);
   const [automatedDateRange, setAutomateDateRange] = useState([
     {
@@ -109,30 +109,7 @@ const Home = () => {
       setCurTab
     );
   };
-  const handleLastConvertedTime = (data) => {
-    if (data) {
-      const dateObj = new Date(data);
 
-      const IST_OFFSET = 5.5 * 60 * 60 * 1000;
-      const istDateObj = new Date(dateObj.getTime() + IST_OFFSET);
-
-      const day = istDateObj.getDate().toString().padStart(2, "0");
-      const month = (istDateObj.getMonth() + 1).toString().padStart(2, "0");
-      const year = istDateObj.getFullYear();
-
-      let hours = istDateObj.getHours();
-      const minutes = istDateObj.getMinutes().toString().padStart(2, "0");
-      const amPm = hours >= 12 ? "PM" : "AM";
-      hours = hours % 12 || 12;
-
-      const formattedDate = `${day}-${month}-${year}`;
-      const formattedTime = `${hours}:${minutes} ${amPm}`;
-
-      return `${formattedTime} , ${formattedDate}`;
-    } else {
-      return " ";
-    }
-  };
   const handleConvertFileDownload = () => {
     fetch(latestConvertedFileUrl)
       .then((response) => response.blob())
@@ -158,12 +135,7 @@ const Home = () => {
       return null;
     }
   };
-  const formatDate = (date) => {
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const year = date.getFullYear();
-    return `${month}/${day}/${year}`;
-  };
+
   const handleAutomate = async () => {
     setshowCalandar(false);
     setIsAutomate(true);
@@ -182,7 +154,6 @@ const Home = () => {
       const result = await triggerApiRequest(data);
 
       if (result.status === 202) {
-        setIsAutomatedApiCalled(true);
         startFetchInterval();
         setIsAutomate(true);
       }
@@ -210,7 +181,7 @@ const Home = () => {
           toast.success("Automation is complete");
           return "stop";
         }
-        if (result.data.status === "Failed") {
+        if (result.data.status === "Failed" || result.data.status === "") {
           setIsAutomate(false);
 
           toast.error("Something went wrong");
--- src/utils/Common.js
+++ src/utils/Common.js
@@ -15,3 +15,37 @@ export const handleSearch = (arr, querys) => {
     });
   });
 };
+
+
+export const handleLastConvertedTime = (data) => {
+  if (data) {
+    const dateObj = new Date(data);
+
+    const IST_OFFSET = 5.5 * 60 * 60 * 1000;
+    const istDateObj = new Date(dateObj.getTime() + IST_OFFSET);
+
+    const day = istDateObj.getDate().toString().padStart(2, "0");
+    const month = (istDateObj.getMonth() + 1).toString().padStart(2, "0");
+    const year = istDateObj.getFullYear();
+
+    let hours = istDateObj.getHours();
+    const minutes = istDateObj.getMinutes().toString().padStart(2, "0");
+    const amPm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+
+    const formattedDate = `${day}-${month}-${year}`;
+    const formattedTime = `${hours}:${minutes} ${amPm}`;
+
+    return `${formattedTime} , ${formattedDate}`;
+  } else {
+    return " ";
+  }
+};
+
+export   const formatDate = (date) => {
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const year = date.getFullYear();
+  return `${month}/${day}/${year}`;
+};
+
